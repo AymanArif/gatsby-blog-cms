611662f5c3d51f1a75394be14729f9cf4f18c069
Ayman Patel <ayman.patel97@gmail.com>
2020-07-08 22:59:43 +0530

    Added header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Home" keywords={['gatsby', 'application', 'react']}/>
     <h1>Hi Ayman </h1>
   </Layout>
 )
